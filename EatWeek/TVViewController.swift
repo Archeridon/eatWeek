4c3282025475c9c4e69e4844a82b189a8dff026b
Maddy Sutton <student@MSutton.local>
2017-02-22 14:32:30 -0600

    Changed so tableView is properly set up

---
+++ EatWeek/TVViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TVViewController.swift
+//  EatWeek
+//
+//  Created by Maddy Sutton on 2/22/17.
+//  Copyright © 2017 Alexander Pacini. All rights reserved.
+//
+
+import UIKit
+
+class TVViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+
+    var days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.days.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell : UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.days[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
+
+}
