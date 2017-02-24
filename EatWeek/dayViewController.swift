adfd8e17be118a6aa12503f5bfee1aaa80614217
Student <Archeridon@bluc02mg3fzfh51.local>
2017-02-24 09:13:45 -0600

    Started Animation Code

--- EatWeek/dayViewController.swift
+++ EatWeek/dayViewController.swift
@@ -9,27 +9,158 @@
 import UIKit
 
 class dayViewController: UIViewController {
-
+    
+    @IBOutlet weak var breakfastImage: UIImageView!
+    @IBOutlet weak var lunchImage: UIImageView!
+    @IBOutlet weak var dinnerImage: UIImageView!
+    @IBOutlet weak var snacksImage: UIImageView!
+    
+    var animationTimer = Timer()
+    var alphaTimer = Timer()
+    
+    var breakCount = 0
+    var lunchCount = -2
+    var dinnerCount = -4
+    var snackCount = -6
+    
+    var breakChange = false
+    var lunchChange = false
+    var dinnerChange = false
+    var snackChange = false
+    
+    var breakAlpha = 1.0
+    var lunchAlpha = 1.0
+    var dinnerAlpha = 1.0
+    var snackAlpha = 1.0
+    
+    var breakBeta = false
+    var lunchBeta = false
+    var dinnerBeta = false
+    var snackBeta = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    animationTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(animationCount), userInfo: nil, repeats: true)
+    
+    alphaTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(alphaAnimation), userInfo: nil, repeats: true)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func animationCount()
+    {
+       
+        if breakCount <= 5{
+        breakCount = breakCount + 1
+        }
+        else
+        {
+           breakChange = true
+        }
+        if lunchCount <= 5{
+            lunchCount = lunchCount + 1
+        }
+        else
+        {
+            lunchChange = true
+        }
+        if dinnerCount <= 5{
+            dinnerCount = dinnerCount + 1
+        }
+        else
+        {
+            dinnerChange = true
+        }
+        if snackCount <= 5{
+            snackCount = snackCount + 1
+        }
+        else
+        {
+            snackChange = true
+        }
+        
     }
-    */
+    
+    func alphaAnimation()
+    {
+        if breakChange == false
+        {}
+        else
+        {
+            if breakBeta == true
+                {
+            breakAlpha = breakAlpha + 0.05
+            breakfastImage.alpha = CGFloat(breakAlpha)
+                }
+            else
+                {
+            breakAlpha = breakAlpha - 0.05
+            breakfastImage.alpha = CGFloat(breakAlpha)
+                }
+            if breakAlpha == 0.0
+            {
+                breakBeta = true
+            }
+        }
+        if lunchChange == false
+        {}
+        else
+        {
+            if breakBeta == true
+                {
+            lunchAlpha = lunchAlpha + 0.05
+            lunchImage.alpha = CGFloat(lunchAlpha)
+                }
+            else
+                {
+            lunchAlpha = lunchAlpha - 0.05
+            lunchImage.alpha = CGFloat(lunchAlpha)
+                }
+            if lunchAlpha == 0.0
+            {
+                lunchBeta = true
+            }
+        }
+        if dinnerChange == false
+        {}
+        else
+        {
+            if breakBeta == true
+                {
+            dinnerAlpha = dinnerAlpha + 0.05
+            dinnerImage.alpha = CGFloat(dinnerAlpha)
+                }
+            else
+                {
+            dinnerAlpha = dinnerAlpha - 0.05
+            dinnerImage.alpha = CGFloat(dinnerAlpha)
+                }
+            if dinnerAlpha == 0.0
+            {
+                dinnerBeta = true
+            }
+        }
+        if snackChange == false
+        {}
+        else
+        {
+            if breakBeta == true
+                {
+            snackAlpha = snackAlpha + 1 0.05
+            snacksImage.alpha = CGFloat(snackAlpha)
+                }
+            else
+                {
+            snackAlpha = snackAlpha - 0.05
+            snacksImage.alpha = CGFloat(snackAlpha)
+                }
+            if snackAlpha == 0.0
+            {
+                snackBeta = true
+            }
+        }
+    }
+
+   
 
 }
